4057269574f6562ceddab99013af49c85fbf0e7a
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-03-24 10:31:54 -0500

    added icons to about section. added container backgrounds to sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,19 @@
 "use strict";
+$(document).ready(function () {
+//bootstrap tooltips
+let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
+
+    const icons = $('.icon');
+
+    icons.mouseover(function () {
+        const icon = $(this);
+        icon.addClass('fa-shake');
+        setTimeout(() => {
+            icon.removeClass('fa-shake');
+        }, 800)
+    })
+})
