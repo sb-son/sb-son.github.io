4ec5b4687f95bc6bb1ed6a2660e38c0531a1d7f2
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-03-24 13:13:04 -0500

    added contact form field using formspree

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,13 @@
 "use strict";
 $(document).ready(function () {
-//bootstrap tooltips
-let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+    //bootstrap tooltips
+    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 
-
+    //icon animation
     const icons = $('.icon');
-
     icons.mouseover(function () {
         const icon = $(this);
         icon.addClass('fa-shake');
@@ -16,4 +15,11 @@ let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
             icon.removeClass('fa-shake');
         }, 800)
     })
+
+    //clear contact form after submission
+    window.onbeforeunload = () => {
+        for(const form of document.getElementsByTagName('form')) {
+            form.reset();
+        }
+    }
 })
