5d9faf2414c3a06cab6adae0e5fefe9f0e3d242f
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-03-27 18:32:25 -0500

    added more images for projects. added styling effects to portfolio cards and revised about me paragraph. added more navigation arrows to sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,49 +16,11 @@ $(document).ready(function () {
         }, 800)
     })
 
+    //for contact form
     window.formbutton = window.formbutton || function () {
         (formbutton.q = formbutton.q || []).push(arguments)
     };
     formbutton("create", {action: "https://formspree.io/f/mpzejovg"})
-    let form = document.getElementById("my-form");
 
-    document.querySelectorAll('.next-section-btn').forEach((button, index, buttons) => {
-        button.addEventListener('click', () => {
-            const currentSection = button.parentElement.parentElement;
-            const nextSection = currentSection.nextElementSibling;
 
-            if (nextSection) {
-                nextSection.scrollIntoView({ behavior: 'smooth' });
-            }
-        });
-    });
-
-    // async function handleSubmit(event) {
-    //     event.preventDefault();
-    //     let status = document.getElementById("my-form-status");
-    //     let data = new FormData(event.target);
-    //     fetch(event.target.action, {
-    //         method: form.method,
-    //         body: data,
-    //         headers: {
-    //             'Accept': 'application/json'
-    //         }
-    //     }).then(response => {
-    //         if (response.ok) {
-    //             status.innerHTML = "Thanks for your submission!";
-    //             form.reset()
-    //         } else {
-    //             response.json().then(data => {
-    //                 if (Object.hasOwn(data, 'errors')) {
-    //                     status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-    //                 } else {
-    //                     status.innerHTML = "Oops! There was a problem submitting your form"
-    //                 }
-    //             })
-    //         }
-    //     }).catch(error => {
-    //         status.innerHTML = "Oops! There was a problem submitting your form"
-    //     });
-    // }
-    // form.addEventListener("submit", handleSubmit)
 })
