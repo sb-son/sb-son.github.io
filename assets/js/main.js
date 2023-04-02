8861df4de2ebafd21b67ce761db340676a5dc1a6
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-04-02 17:37:44 -0500

    added description to the projects array objects. appended project desc to the html list item for each project

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,26 +62,33 @@ $(document).ready(function () {
     $(".portfolio-item").click(function () {
         $(this).find(".portfolio-info").slideToggle();
     });
-    
+
+    //an array of projects that makes use of svgs in svg.js
     const projects = [
         {
             id: 'sandlot-connect',
             svg: createSandlotConnectSvg(),
+            desc: `Project Name: Sandlot Connect\n
+             Sandlot Connect is a social media platform inspired by the grassroots resurgence of sandlot baseball across Texas and beyond. Within this web application, users can create a new baseball team or request to join an existing team. When a user creates a new team they have access to requests and can approve or dismiss requests to join the team. Once on a team, members have a private space where they can make a post and communicate with group members. The application is built using Java and Spring Boot on the back-end, with Thymeleaf for the front-end.`
         },
         {
             id: 'movie-project',
             svg: createMovieProjectSvg(),
+            desc: `Project Name: Movie Project\n
+                    This project is a movie application that allows users to add, edit, and delete movies, as well as rate them. It features a user-friendly interface and integrates with OMDB API for movie posters. The app is built with HTML, CSS, JavaScript, and jQuery for a responsive user experience and uses fetch API and RESTful APIs to communicate with a server for storage of movie data.`
         },
         {
             id: 'weather-map',
             svg: createWeatherMapSvg(),
+            desc: `Project Name: Weather Map\n
+                    This project is a movie application that allows users to add, edit, and delete movies, as well as rate them. It features a user-friendly interface and integrates with OMDB API for movie posters. The app is built with HTML, CSS, JavaScript, and jQuery for a responsive user experience and uses fetch API and RESTful APIs to communicate with a server for storage of movie data.`
         }
     ];
 
     projects.forEach(project => {
-        const listItem = $(`<li class="svg-container-${project.id}">`)
-        console.log(project)
-        listItem.html(project.svg)
+        const listItem = $(`<li class="svg-container-${project.id} d-grid">`)
+        listItem.html(project.svg);
+        listItem.append(`<p>${project.desc}</p>`);
         $('.portfolio-container ul').append(listItem)
 
     })
